995855de1af89153d3ae52150f2612e7d9630061
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-04 20:28:13 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,9 +60,7 @@ export default async () => {
       { src: '~/plugins/directives', mode: 'client' },
       { src: '~/plugins/swell-lang.js' },
       { src: '~/plugins/filters.js' },
-      { src: '~/plugins/font-sampler/dist/fontsampler.js' },
-      { src: '~/plugins/font-sampler/dist/fontsampler.min.js' },
-      { src: '~/plugins/font-sampler/dist/loader.js' },
+
     ],
 
     /*
