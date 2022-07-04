f305dad6abcf4d4ce8b78c441498eb1852321cbd
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-04 22:17:59 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,9 @@ export default async () => {
     components: true,
    head:{
     script: [
-      { src: 'js/fontsampler-skin.js', type: 'text/javascript', body: true, defer: true },
-      { src: 'js/fontsampler.js', type: 'text/javascript', body: true, defer: true },
-      { src: 'js/loader.js', type: 'text/javascript', body: true, defer: true }
+      { src: 'https://font-sampler.vercel.app/dist/fontsampler-skin.js', type: 'text/javascript', body: true, defer: true },
+      { src: 'https://font-sampler.vercel.app/dist/fontsampler.js', type: 'text/javascript', body: true, defer: true },
+      { src: 'https://font-sampler.vercel.app/dist/loader.js', type: 'text/javascript', body: true, defer: true }
    ]
    },
 
