d0b05a44c0271206727452de48048be97c2dddc0
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 00:06:32 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,9 @@ export default async () => {
     components: true,
    head:{
     script: [
-      { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler-skin.js', type: 'text/javascript' },
-      { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js', type: 'text/javascript'},
-      { src: 'https://www.oztype.com/Font-Sampler/dist/loader.js', type: 'text/javascript'}
+      { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler-skin.js', type: 'text/javascript', body: true, defer: true },
+      { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js', type: 'text/javascript', body: true, defer: true},
+      { src: 'https://www.oztype.com/Font-Sampler/dist/loader.js', type: 'text/javascript', body: true, defer: true}
    ]
    },
 
