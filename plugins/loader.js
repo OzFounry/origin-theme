ad9fd8836851ba76973569758e252d1e9c6152e5
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 12:42:20 +0100

    Update loader.js

--- plugins/loader.js
+++ plugins/loader.js
@@ -1,6 +1,6 @@
 export default ({ app: { head, router, context } }, inject) => {
     head.scripts.push({
       src: 'https://www.oztype.com/Font-Sampler/dist/loader.js'
-      defer: true
+
     })
   })
