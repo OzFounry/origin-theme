f99db82ad2b1799b044a5e26f77f74ff1bc546ba
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 12:54:35 +0100

    Delete fontsampler.js

--- plugins/fontsampler.js
+++
@@ -1,6 +0,0 @@
-export default ({ app: { head, router, context } }, inject) => {
-    head.scripts.push({
-      src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js' 
-      
-    })
-  })
