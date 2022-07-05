d0d29eb4c1d59e7ac0957bb912b7d11e09923ce3
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 12:41:32 +0100

    Update fontsampler.js

--- plugins/fontsampler.js
+++ plugins/fontsampler.js
@@ -1,6 +1,6 @@
 export default ({ app: { head, router, context } }, inject) => {
     head.scripts.push({
       src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js' 
-      defer: true
+      
     })
   })
