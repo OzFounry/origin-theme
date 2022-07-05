3c6fa07fb861697a9de385c424c0a982d75ce973
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 12:54:18 +0100

    Update loader.js

--- plugins/loader.js
+++ plugins/loader.js
@@ -1,6 +1,5 @@
 export default ({ app: { head, router, context } }, inject) => {
-    head.scripts.push({
-      src: 'https://www.oztype.com/Font-Sampler/dist/loader.js'
-
-    })
+    head:{ script: [ { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js', type: 'text/javascript', body: true, defer: true, mode: 'client' }, 
+                    { src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler-skin.js', type: 'text/javascript', body: true, defer: true, mode: 'client'}, 
+                    { src: 'https://www.oztype.com/Font-Sampler/dist/loader.js', type: 'text/javascript', body: true, defer: true, mode: 'client'} ] },
   })
