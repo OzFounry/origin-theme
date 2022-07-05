494f02951f9a448008b11a0de39729e5ae226485
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-07-05 12:04:35 +0100

    Add files via upload

---
+++ plugins/fontsampler.js
@@ -0,0 +1,7 @@
+export default ({ app: { head, router, context } }, inject) => {
+    head.scripts.push({
+      src: 'https://www.oztype.com/Font-Sampler/dist/fontsampler.js'
+      async: false,
+      defer: true
+    })
+  })
